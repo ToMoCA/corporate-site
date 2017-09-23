12aa07c6f163ff885563d2802194349660638fa9
Shinji TAKENAKA <ks6088ts@gmail.com>
2017-09-23 09:34:43 +0900

    Add test data of products

--- scripts/setup_django.sh
+++ scripts/setup_django.sh
@@ -7,4 +7,6 @@ cd $SCRIPT_DIR/../project/
 ./manage.py makemigrations
 ./manage.py migrate
 ./manage.py createsuperuser
+# ./manage.py dumpdata products --format=json --indent=2 > products.json
+./manage.py loaddata products
 ./manage.py runserver 0.0.0.0:8080

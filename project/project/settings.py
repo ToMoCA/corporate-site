d59b5d01e9341e73b5c194f19ba6ed1adffa5b51
Shinji TAKENAKA <ks6088ts@gmail.com>
2017-10-07 16:35:24 +0900

    Use intcomma module to improve price description (ref: https://qiita.com/KamekoKameKame/items/115f054e868593a4189f)

--- project/project/settings.py
+++ project/project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'products.apps.ProductsConfig',
     'ckeditor',
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
